4b1b546a7349fbd0604feacab3e6e50d70405a07
Jordan Rosas <jordanrosas1203@gmail.com>
2023-02-19 18:58:15 -0600

    more content

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-$().ready(() => console.log("working"))
+$(document).ready(function () {
+    $('.main').attr({
+        "data-aos": "zoom-in",
+        "data-aos-duration": "2000"
+    });
+    setTimeout(() => {
+        AOS.init();
+    }, 120);
+
+
+});
 
